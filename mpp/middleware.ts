d9968e2a72818f2b90a025807d38bc0bb25c2b0c
Robert076 <robertberes06@gmail.com>
2025-03-25 09:36:22 +0200

    middleware working

--- mpp/middleware.ts
+++ mpp/middleware.ts
@@ -1,5 +1,14 @@
-import { NextRequest } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 
 export default function middleware(request: NextRequest) {
+    const sessionToken = request.cookies.get("authjs.session-token") || request.cookies.get("__Secure-authjs.session-token");
+    if(!sessionToken) {
+        return NextResponse.redirect(new URL("/login", request.url));
+    }
 
+    return NextResponse.next();
 }
+
+export const config = {
+    matcher: ["/dashboard"],
+  };
